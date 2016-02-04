da7b1995ec6ea3a620ead579c900962fed64953e
Thomas Tucker <elteano@gmail.com>
2016-02-04 12:10:13 -0800

    Added toast message to give register button feedback.

---
+++ public/js/login.js
@@ -0,0 +1,33 @@
+'use strict';
+
+$(document).ready(function() {
+	initializePage();
+})
+
+function initializePage() {
+	$('#register').click(reg_func);
+}
+
+function reg_func(e)
+{
+
+	toastr.options = {
+		"closeButton": false,
+		"debug": false,
+		"newestOnTop": false,
+		"progressBar": false,
+		"positionClass": "toast-bottom-center",
+		"preventDuplicates": false,
+		"onclick": null,
+		"showDuration": "1000",
+		"hideDuration": "1000",
+		"timeOut": "1500",
+		"extendedTimeOut": "1000",
+		"showEasing": "swing",
+		"hideEasing": "linear",
+		"showMethod": "fadeIn",
+		"hideMethod": "fadeOut"
+	};
+	toastr.info('Welcome to Catchi!');
+}
+
