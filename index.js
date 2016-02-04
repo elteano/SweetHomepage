2caee049e6ffe3565768ff97a0037b0c3da83477
Thomas Tucker <elteano@gmail.com>
2016-02-03 23:58:35 -0800

    Added text edit screen.

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var handlebars = require('express3-handlebars')
 var index = require('./routes/index');
 var system = require('./routes/system');
 var list = require('./routes/list');
+var textedit = require('./routes/textedit');
 
 var app = express();
 app.set('port', (process.env.PORT || 3000));
@@ -18,6 +19,7 @@ app.use(express.static(__dirname + '/public'));
 app.get('/', index.view);
 app.get('/system', system.view);
 app.get('/list', list.view);
+app.get('/textedit', textedit.view);
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
