bbca9c1b89ce3e1baf3a392b4ac8ab6a0379f45a
Divya Aggarwal <daggarwa@ucsd.edu>
2016-02-04 21:10:40 -0800

    changes for search

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ var list = require('./routes/list');
 var textedit = require('./routes/textedit');
 var edit_menu = require('./routes/edit_menu');
 var drawedit = require('./routes/drawedit');
+var search = require('./routes/search');
 
 var app = express();
 app.set('port', (process.env.PORT || 3000));
@@ -24,6 +25,7 @@ app.get('/list', list.view);
 app.get('/textedit', textedit.view);
 app.get('/edit', edit_menu.view);
 app.get('/drawedit', drawedit.view);
+app.get('/search', search.view);
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
