c499db75f30839f496eb30ba610c9af4ce37dc07
Thomas Tucker <elteano@gmail.com>
2016-02-11 17:57:57 -0800

    Now lists all planets according to the search and will redirect to the system view based on the result.

--- public/js/search.js
+++ public/js/search.js
@@ -4,21 +4,24 @@ $(document).ready(function() {
 	initializePage();
 });
 
+var current_arr = [];
+
 function initializePage()
 {
 	$.get('/sysinfo', planet_callback);
 	$('#search').click(search_click);
 	$('#searchbar').on('input', search_input_changed);
+	$('.systemplanet').click(planet_click);
 }
 
 var search_toggle = false;
 
 function planet_callback(response)
 {
-	console.log(response);
+	current_arr = response;
 	var newhtml = aggregate_planets(response);
-	console.log(newhtml);
 	$('.listcontainer').html(newhtml);
+	$('.systemplanet').click(planet_click);
 }
 
 function search_click(e)
@@ -45,11 +48,11 @@ function search_input_changed(e)
 	$.get(url, planet_callback);
 }
 
-function get_planet_html(obj)
+function get_planet_html(obj, id)
 {
 	var width = $('.listcontainer').width() / 2;
 	var html = '<div class="systemplanet" style="border-color: #' + obj.color +
-		';"><span>' + obj.name + '</span></div>'
+		';"><span>' + obj.name + '</span><div class="id">' + id + '</div></div>'
 	return html;
 }
 
@@ -60,7 +63,7 @@ function aggregate_planets(obj_list)
 	var height = $('.listcontainer').width() / 2;
 	for (; i < obj_list.length; i++)
 	{
-		var planet_html = get_planet_html(obj_list[i]);
+		var planet_html = get_planet_html(obj_list[i], i);
 		if (i % 2 == 0)
 		{
 			html += '<div class="listrow">';
@@ -76,5 +79,12 @@ function aggregate_planets(obj_list)
 		html += '<div class="planet_container"></div></div>';
 	}
 	return html;
+}
+
+function planet_click(e)
+{
+	e.preventDefault();
+	console.log($(this).find('.id').text());
+	window.location.href = '/system';
 }
 
