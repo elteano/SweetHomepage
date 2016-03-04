38337f2a9c88ec45f6ac34d1734c78af98c8f965
Divya Aggarwal <daggarwa@ucsd.edu>
2016-03-03 19:14:09 -0800

    Splash screen

--- public/js/login.js
+++ public/js/login.js
@@ -1,6 +1,6 @@
 'use strict';
 
-toastr.options = {
+/*toastr.options = {
 	"closeButton": false,
 	"debug": false,
 	"newestOnTop": false,
@@ -16,81 +16,91 @@ toastr.options = {
 	"hideEasing": "linear",
 	"showMethod": "fadeIn",
 	"hideMethod": "fadeOut"
-};
+};*/
 
-var userarr = [
-	{
-		"user": "albert",
-		"pass": "supersecure"
-	},
-	{
-		"user": "asdf",
-		"pass": "fdsa"
-	},
-];
+// var userarr = [
+// 	{
+// 		"user": "albert",
+// 		"pass": "supersecure"
+// 	},
+// 	{
+// 		"user": "asdf",
+// 		"pass": "fdsa"
+// 	},
+// ];
 
 $(document).ready(function() {
-	initializePage();
+	
+    $('#splashscreen').fadeOut(5000);
+    //$('#system').fadeIn(200);
+    // var nextPage = window.location.href;
+    // nextPage = '/system?sys=0';
+    function loadNewPage() {
+    	window.location.href = '/system?sys=0';
+    	console.log("loading");
+    }
+    window.setTimeout(loadNewPage, 5100);
+
 })
 
-function initializePage() {
-	$('#register').click(reg_func);
-	$('#login').click(on_login);
-}
+// function initializePage() {
+// 	$('#register').click(reg_func);
+// 	$('#login').click(on_login);
+// }
 
-function reg_func(e)
-{
-	var user = $('#email').val();
-	var pass = $('#pass').val();
-	if (test_user_validity(user))
-	{
-		userarr.push({
-				"user": user,
-				"pass": pass
-				});
-		toastr.success('Welcome to Catchi!');
-	}
-	else
-	{
-		toastr.error('This username is already taken!');
-	}
-}
+// function reg_func(e)
+// {
+// 	var user = $('#email').val();
+// 	var pass = $('#pass').val();
+// 	if (test_user_validity(user))
+// 	{
+// 		userarr.push({
+// 				"user": user,
+// 				"pass": pass
+// 				});
+// 		toastr.success('Welcome to Catchi!');
+// 	}
+// 	else
+// 	{
+// 		toastr.error('This username is already taken!');
+// 	}
+// }
 
-function test_user_validity(user)
-{
-	for (var i = 0, len=userarr.length; i < len; i++)
-	{
-		if (user == userarr[i].user)
-		{
-			return false;
-		}
-	}
-	return true;
-}
+// function test_user_validity(user)
+// {
+// 	for (var i = 0, len=userarr.length; i < len; i++)
+// 	{
+// 		if (user == userarr[i].user)
+// 		{
+// 			return false;
+// 		}
+// 	}
+// 	return true;
+// }
 
-function on_login(e)
-{
-	e.preventDefault();
-	var user = $('#email').val();
-	var pass = $('#pass').val();
-	var found = false;
+// function on_login(e)
+// {
+// 	e.preventDefault();
+// 	var user = $('#email').val();
+// 	var pass = $('#pass').val();
+// 	var found = false;
 
-	for (var i = 0, len=userarr.length; i < len; i++)
-	{
-		if (user == userarr[i].user)
-		{
-			if (pass == userarr[i].pass)
-			{
-				toastr.success('Welcome back!');
-				found = true;
-				window.location.href = '/system?sys=0';
-			}
-		}
-	}
+// 	for (var i = 0, len=userarr.length; i < len; i++)
+// 	{
+// 		if (user == userarr[i].user)
+// 		{
+// 			if (pass == userarr[i].pass)
+// 			{
+// 				toastr.success('Welcome back!');
+// 				found = true;
+// 				window.location.href = '/system?sys=0';
+// 			}
+// 		}
+// 	}
 
-	if (!found)
-	{
-		toastr.error('Username and password combination not valid.');
-	}
-}
+// 	if (!found)
+// 	{
+// 		toastr.error('Username and password combination not valid.');
+// 	}
+// }
 
